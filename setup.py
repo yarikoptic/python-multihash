53bd11f49ae350bc28d79b0fa305f564ebf6e94a
bmcorser <bmcorser@gmail.com>
2015-04-16 22:27:27 +0100

    py.test integration

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
+import sys
 import setuptools
+from setuptools.command.test import test as TestCommand
 
-VERSION = '0.0.0'
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+VERSION = '0.0.1'
 
 setuptools.setup(
     name='multihash',
@@ -9,4 +29,6 @@ setuptools.setup(
     author_email='bmcorser@gmail.com',
     version=VERSION,
     packages=setuptools.find_packages(),
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
